915c2fb6e73aea7c8398826a5925a5b9f4701040
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-03-05 07:43:40 -0600

    finish imagelink form, adding rank and cobweb background

--- 01_Front_End/src/App.jsx
+++ 01_Front_End/src/App.jsx
@@ -4,14 +4,18 @@ import { Component } from 'react'
 import Navigation from './Components/Navigation/Navigation.jsx'
 import Logo from './Components/Logo/logo.jsx'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.jsx'
+import Rank from './Components/Rank/Rank.jsx'
 import 'tachyons'
+import ParticlesBG from 'particles-bg'
 
 class App extends Component {
   render () {
     return (
-      <div classname='App'>
+      <div className='App'>
+        <ParticlesBG className="particles" type="cobweb" bg={true} color="FFFFFF" />
         <Navigation />
         <Logo />
+        <Rank/>
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
