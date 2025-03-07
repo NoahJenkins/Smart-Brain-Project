dfb8716eea86466179b663112205ae83be30739d
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-03-07 16:31:48 -0600

    box highlighting face

--- 01_Front_End/src/Components/FaceRecognition/FaceRecognition.jsx
+++ 01_Front_End/src/Components/FaceRecognition/FaceRecognition.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
-        <div className='center ma'>
-            <img alt='' src={imageUrl} width='500px' height='auto'/>
+        <div className='center ma pt4'>
+            <div className='relative'>
+                <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
+                {box && (
+                    <div className='bounding-box' style={{
+                        top: box.topRow, 
+                        right: box.rightCol, 
+                        bottom: box.bottomRow, 
+                        left: box.leftCol
+                    }}></div>
+                )}
+            </div>
         </div>
     )
 };
