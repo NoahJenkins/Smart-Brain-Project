e27733018bc083ef1b1f831b3fb83aa68d0a1103
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-03-15 09:10:46 -0500

    APi endpoints

--- 02_Back_End/server.js
+++ 02_Back_End/server.js
@@ -1,9 +1,80 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const e = require('express');
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: "sally@gmail.com",
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]};
+
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    res.send(database.users);
+});
+
+app.post('/signin', (req,res) => {
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json('success');
+  } else {
+    res.json('fail');
+  }
+});
+
+app.post('/register', (req,res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length-1]);
+});
+
+app.get('/profile/:id', (req,res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  
+  if (!found) {
+    res.status(404).json('not found');
+  }
+});
+
+app.post('/image', (req,res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true
+      user.entries++
+      return res.json(user.entries);
+    }
+  })
 });
 
 app.listen(3000, () => {
